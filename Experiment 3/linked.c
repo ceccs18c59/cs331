ba25fef027a9e47c65c97545174d5f1866c60513
Thejus R <chn18cs118@ceconline.edu>
2020-12-22 20:24:21 +0530

    Finished Linked File Allocation

---
+++ Experiment 3/linked.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void allocateFile(int file[])
+{
+    int st, len, k, c, j;
+    printf("\nEnter the index of the starting block and its length: ");
+    scanf("%d %d", &st, &len);
+    k = len;
+    if (file[st] == 0)
+    {
+        for (j = st; j < (st + k); j++)
+        {
+            if (file[j] == 0)
+            {
+                file[j] = 1;
+                printf("%d------>%d\n", j, file[j]);
+            }
+            else
+            {
+                printf("The block %d is already allocated \n", j);
+                k++;
+            }
+        }
+    }
+    else
+        printf("The block %d is already allocated \n", st);
+    printf("\nDo you want to enter more files? \n");
+    printf("Enter 1 for Yes, Enter 0 for No: ");
+    scanf("%d", &c);
+    if (c == 1)
+        allocateFile(file);
+    else
+        exit(0);
+    return;
+}
+
+int main()
+{
+    int file[50], p, a;
+
+    for (int i = 0; i < 50; i++)
+        file[i] = 0;
+    printf("Enter the number of blocks already allocated: ");
+    scanf("%d", &p);
+    printf("Enter the blocks already allocated: ");
+    for (int i = 0; i < p; i++)
+    {
+        scanf("%d", &a);
+        file[a] = 1;
+    }
+
+    allocateFile(file);
+    getch();
+    return 0;
+}
