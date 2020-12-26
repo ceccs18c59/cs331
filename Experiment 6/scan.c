84e1928baf4ce425ac776c3eb4cf4374f33075c9
Thejus R <thejusr1999@gmail.com>
2020-12-27 00:11:42 +0530

    Updated scan.c and scan.exe

--- Experiment 6/scan.c
+++ Experiment 6/scan.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <conio.h>
+#include <math.h>
 
-int sortSeq(int p[], int limit)
+int sortSeq(int seq[], int limit)
 {
     int pos, temp;
     for (int i = 0; i < limit; i++)
@@ -9,19 +10,63 @@ int sortSeq(int p[], int limit)
         pos = i;
         for (int j = i; j < limit; j++)
         {
-            if (p[j] < p[pos])
+            if (seq[j] < seq[pos])
                 pos = j;
         }
-        temp = p[i];
-        p[i] = p[pos];
-        p[pos] = temp;
+        temp = seq[i];
+        seq[i] = seq[pos];
+        seq[pos] = temp;
     }
     return 0;
 };
 
+int findIndex(int seq[], int limit, int head)
+{
+    for (int i = 0; i < limit; i++)
+        if (head == seq[i])
+        {
+            seq[i] = 0;
+            return i;
+            break;
+        }
+}
+int seeker(int seq[], int limit, int head, int direction, int init)
+{
+    int count = 0, j = 0, i, seqL[20], seekTime = 0;
+    int run = 2;
+    while (run--)
+    {
+        if (direction == 0)
+        {
+            for (int i = head - 1; i >= 0; i--)
+            {
+                seqL[count] = seq[i];
+                printf("%d ", seq[i]);
+                count++;
+            }
+            direction = 1;
+        }
+        else if (direction == 1)
+        {
+            for (int i = head; i <= limit; i++)
+            {
+                seqL[count] = seq[i];
+                printf("%d ", seq[i]);
+                count++;
+            }
+            direction = 0;
+        }
+    }
+    seekTime = init - seqL[0];
+    for (int i = 0; i < limit; i++)
+        seekTime = seekTime + abs(seqL[i] - seqL[i + 1]);
+    printf("\n Total Seek Time : %d", seekTime);
+    return 0;
+}
+
 void main()
 {
-    int seq[20], i, seekTime = 0, limit;
+    int seq[20], seqL[20], i, j, seekTime = 0, limit, init;
     int direction;
 
     printf("Enter lenght of Sequence: ");
@@ -32,34 +77,16 @@ void main()
         scanf("%d", &seq[i + 1]);
 
     printf("Enter initial head position: ");
-    scanf("%d", &seq[0]);
+    scanf("%d", &init);
 
-    sortSeq(seq, limit);
+    seq[0] = init;
 
-    printf("\nSorted Sequence : ");
-    for (i = 0; i < limit + 1; i++)
-        printf("%d ", seq[i]);
+    sortSeq(seq, limit + 1);
+    int head = findIndex(seq, limit + 1, init);
 
-    /*
-    printf("Enter the Direction (Right 0, Left 1): ");
-    scanf("%d", &direction);
+    // 0 = left, 1 = right
+    printf("\nSeek Sequence: ");
+    seeker(seq, limit, head, 0, init);
 
-
-
-    switch (direction)
-    {
-    case 1:
-        printf("Left Direction");
-        break;
-
-    case 0:
-        printf("rigjt");
-        break;
-    default:
-        break;
-    }
-    */
-
-    //code
     getch();
 }
